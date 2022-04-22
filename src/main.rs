7355a5278e6de11fe0b5d773bcb851ddb30f1d54
craigfay <craigfay1@gmail.com>
2022-04-21 19:42:44 -0500

    Use command line args

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,9 @@ struct Config {
 }
 
 fn main() {
-    let path = "config.json";
+    let path = std::env::args().nth(1)
+        .unwrap_or("config.json".to_string());
+
     let file = File::open(path).unwrap();
     let reader = BufReader::new(file);
     let config: Config = serde_json::from_reader(reader).unwrap();
