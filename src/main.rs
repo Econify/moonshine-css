dcfbdad514a3ed8f8fa4483fb5ec76da6f2d286f
craigfay <craigfay1@gmail.com>
2022-04-21 19:39:53 -0500

    Add config properties

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,18 @@ use std::fs::File;
 use std::io::BufReader;
 
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 struct Config {
     color: HashMap<String, String>,
+    font: HashMap<String, String>,
+    gradient: HashMap<String, String>,
+    font_size: Vec<f32>,
+    line_height: Vec<f32>,
+    opacity: Vec<u32>,
+    border_radius: Vec<u32>,
+    paragraph_spacing: Vec<u32>,
+    letter_spacing: Vec<u32>,
+    spacing: Vec<u32>,
 }
 
 fn main() {
