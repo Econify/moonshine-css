6306f51e2969eb9755a48d493bc3fb3e0ca855d1
craigfay <craigfay1@gmail.com>
2022-05-25 12:57:26 -0500

    Move file src string into const variable

--- distill/src/init.rs
+++ distill/src/init.rs
@@ -1,15 +1,7 @@
 use std::fs;
 use std::path::Path;
 
-pub fn initialize_moonshinerc(path: &str) {
-    if Path::new(path).exists() {
-        println!("File \"{}\" already exists", path);
-        return;
-    }
-
-    println!("Initializing .moonshinerc");
-
-    let text = r#"
+const RC_FILE_SRC: &str = r#"
 {
   "designTokens": [
     "./advanced/atomic-styles.design-tokens.json"
@@ -24,7 +16,15 @@ pub fn initialize_moonshinerc(path: &str) {
 }
 "#;
 
-    fs::write(path, text).expect("Unable to write .moonshinerc to current directory");
+pub fn initialize_moonshinerc(path: &str) {
+    if Path::new(path).exists() {
+        println!("File \"{}\" already exists", path);
+        return;
+    }
+
+    println!("Initializing .moonshinerc");
+
+    fs::write(path, RC_FILE_SRC).expect("Unable to write .moonshinerc to current directory");
 
     println!("\x1b[32mDone\x1b[0m - now run 'distill' to start using Moonshine CSS");
 }
