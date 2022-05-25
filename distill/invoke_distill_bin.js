18e8c8c77f2b3a791440d9e78e9f41b0fc20712d
Thomas Weustenfeld <tw@econify.com>
2022-05-24 23:22:47 -0500

    Formated more files

--- distill/invoke_distill_bin.js
+++ distill/invoke_distill_bin.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 
-const spawn = require('child_process').spawn;
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
-
+const spawn = require("child_process").spawn;
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
 
 const binaryPaths = {
-    "Linux": path.join(__dirname, "target/release/distill"),
-    "Windows_NT": path.join(__dirname, "target/release/distill"),
-    "Darwin": path.join(__dirname, "target/release/distill"),
+  Linux: path.join(__dirname, "target/release/distill"),
+  Windows_NT: path.join(__dirname, "target/release/distill"),
+  Darwin: path.join(__dirname, "target/release/distill"),
 };
 
 // Collecting command line arguments
@@ -17,10 +16,10 @@ const [_interpreter, _parentCommand, ...args] = process.argv;
 
 let binaryPath = binaryPaths[os.type()];
 
-if (!fs.existsSync(binaryPath)){
-  throw new Error('Unsupported operating system!');
-} 
+if (!fs.existsSync(binaryPath)) {
+  throw new Error("Unsupported operating system!");
+}
 
 const childProcess = spawn(binaryPath, args);
-childProcess.stdout.on('data', (data) => console.log(data.toString()));
-childProcess.stderr.on('data', (data) => console.log(data.toString()));
+childProcess.stdout.on("data", (data) => console.log(data.toString()));
+childProcess.stderr.on("data", (data) => console.log(data.toString()));
